4c2459146609911d4e7d318f938991efd9590a55
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-10-10 17:16:00 -0700

    building events model

--- server/models/Event.js
+++ server/models/Event.js
@@ -4,7 +4,7 @@ const dateFormat = require('../utils/dateFormat');
 const eventSchema = new Schema({
   eventText: {
     type: String,
-    required: 'You need to leave a thought!',
+    // required: 'You need to leave a thought!',
     minlength: 1,
     maxlength: 500,
     trim: true,
@@ -14,18 +14,18 @@ const eventSchema = new Schema({
     required: true,
     trim: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  //   get: (timestamp) => dateFormat(timestamp),
+  // },
   comments: [
     {
       commentText: {
         type: String,
         required: true,
         minlength: 1,
-        maxlength: 280,
+        maxlength: 500,
       },
       commentAuthor: {
         type: String,
