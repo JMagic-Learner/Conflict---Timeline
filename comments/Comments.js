cf061c597192e9d5ead4bf4fbb0c4558c4bf5410
JasonDesktop <MaJason93@gmail.com>
2021-10-16 14:25:17 -0700

    heroku deploy test

---
+++ comments/Comments.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import CommentForm from "./CommentForm";
+import Comment from "./Comment";
+// import {
+//   getComments as getCommentsApi,
+//   createComment as createCommentApi,
+//   updateComment as updateCommentApi,
+//   deleteComment as deleteCommentApi,
+// } from "../api";
+
+
+// Importing premade data.
+// Comments.js is the component that will be rendered into SingleEvent.js
+// HIERARCHY TABLE:
+// EXPORT TOP: Comments.js
+// IMPORTING INTO TOP: Comment.js
+// IMPORTING INTO MIDDLE: CommentForm.js ---> Comment.js
+
+
+const Comments = ({ commentsUrl, currentUserId }) => {
+  const [backendComments, setBackendComments] = useState([]);
+  const [activeComment, setActiveComment] = useState(null);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
+  const getReplies = (commentId) =>
+    backendComments
+      .filter((backendComment) => backendComment.parentId === commentId)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
+  const addComment = (text, parentId) => {
+    createCommentApi(text, parentId).then((comment) => {
+      setBackendComments([comment, ...backendComments]);
+      setActiveComment(null);
+    });
+  };
+
+  const updateComment = (text, commentId) => {
+    updateCommentApi(text).then(() => {
+      const updatedBackendComments = backendComments.map((backendComment) => {
+        if (backendComment.id === commentId) {
+          return { ...backendComment, body: text };
+        }
+        return backendComment;
+      });
+      setBackendComments(updatedBackendComments);
+      setActiveComment(null);
+    });
+  };
+  const deleteComment = (commentId) => {
+    if (window.confirm("Are you sure you want to remove comment?")) {
+      deleteCommentApi().then(() => {
+        const updatedBackendComments = backendComments.filter(
+          (backendComment) => backendComment.id !== commentId
+        );
+        setBackendComments(updatedBackendComments);
+      });
+    }
+  };
+
+  useEffect(() => {
+    getCommentsApi().then((data) => {
+      setBackendComments(data);
+    });
+  }, []);
+
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comment-form-title">Write comment</div>
+      <CommentForm submitLabel="Write" handleSubmit={addComment} />
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <Comment
+            key={rootComment.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
+            activeComment={activeComment}
+            setActiveComment={setActiveComment}
+            addComment={addComment}
+            deleteComment={deleteComment}
+            updateComment={updateComment}
+            currentUserId={currentUserId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
